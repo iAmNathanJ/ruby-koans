1eb01e9ab7afa01efeafbecc22322819ab1345ca
iAmNathanJ <jacobs@heysparkbox.com>
2016-01-18 16:55:51 -0500

    triangle.rb part 2

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+
+  if invalid_triangle?([a, b, c])
+    raise TriangleError
+  end
+
   if(a == b && a == c)
     :equilateral
   elsif(a == b || a == c || b == c)
@@ -21,6 +26,24 @@ def triangle(a, b, c)
   else
     :scalene
   end
+end
+
+def invalid_triangle?(sides)
+
+  # Check for zero or negative sides
+  sides.each do |side|
+    return true if side <= 0
+  end
+
+  # Check each side is less than the sum of adjacent sides
+  for i in sides
+    if sides[0] >= sides[1] + sides[2]
+      return true
+    end
+    sides.rotate!(1)
+  end
+
+  false
 end
 
 # Error class used in part 2.  No need to change this code.
