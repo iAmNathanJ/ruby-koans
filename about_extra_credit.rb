908dd284dcfb8ee26fdbedeb6fdb99353ea36406
iAmNathanJ <jacobs@heysparkbox.com>
2016-01-21 13:32:35 -0500

    update .gitignore: remove dice_game

--- about_extra_credit.rb
+++ about_extra_credit.rb
@@ -6,3 +6,9 @@
 # You already have a DiceSet class and score function you can use.
 # Write a player class and a Game class to complete the project.  This
 # is a free form assignment, so approach it however you desire.
+
+########################################
+# Dice Game
+########################################
+
+# See => dice_game/
